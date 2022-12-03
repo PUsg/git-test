372a1c704439de1a3db9ab44796f113478d84240
Puc <2353559384@qq.com>
2022-12-03 21:29:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.atguigu.git
+ * @Author: Top King
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
