4646a5a3c8ad301e648987f72e23870ae201c400
Puc <2353559384@qq.com>
2022-12-03 21:32:27 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,10 +6,14 @@ package com.atguigu.git;
  * @Author: Top King
  * @Description:
  */
+
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+
     }
 
 }
